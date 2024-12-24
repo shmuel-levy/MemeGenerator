812f64cdaf1ebe7500716535176c73e61cfa166a
shmuel-levy <shmuellevy16@gmail.com>
2024-12-24 19:02:17 +0200

    updated saved-memes, and added media query

--- JS/controllers/gallery-controller.js
+++ JS/controllers/gallery-controller.js
@@ -43,6 +43,32 @@ function updateKeywordsMap(searchTerm) {
     renderKeywordSearchDatalist()
 }
 
+function renderSavedMemes() {
+    const savedMemes = getSavedMemes()
+    const containerEl = document.querySelector('.saved-memes-container')
+    if (!containerEl) return
+    
+    const strHTML = savedMemes.map(meme => `
+        <div class="saved-meme">
+            <img src="${meme.imageData}" 
+                 onclick="onLoadSavedMeme('${meme.id}')" 
+                 alt="saved meme">
+        </div>
+    `)
+    
+    containerEl.innerHTML = strHTML.join('')
+}
+
+function onLoadSavedMeme(memeId) {
+    const savedMemes = getSavedMemes()
+    const meme = savedMemes.find(meme => meme.id === memeId)
+    if (meme) {
+        gMeme = { ...meme.memeState }
+        showSection('meme-editor')
+        renderMeme()
+    }
+}
+
 function renderKeywordSearchDatalist() {
     const keywords = Object.keys(gKeywordSearchCountMap)
         .sort((a, b) => gKeywordSearchCountMap[b] - gKeywordSearchCountMap[a])
